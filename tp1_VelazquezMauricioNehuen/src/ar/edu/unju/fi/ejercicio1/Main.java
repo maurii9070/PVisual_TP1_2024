233e8f547c1567a7523d44c762be42ed654a380e
Mauricio Velazquez <mauri_i9070@hotmail.com>
2024-04-03 19:42:08 -0300

    Ejercicio 1 Resuelto.

--- tp1_VelazquezMauricioNehuen/src/ar/edu/unju/fi/ejercicio1/Main.java
+++ tp1_VelazquezMauricioNehuen/src/ar/edu/unju/fi/ejercicio1/Main.java
@@ -3,7 +3,17 @@ package ar.edu.unju.fi.ejercicio1;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Programacion Visual 2024");
+		int num1 = 8;
+		int num2 = 5;
+		int num3 = 1;
+		int num4 = 20;
+		int num5 = 9;
+		int suma;
+		
+		suma = num1 + num2 + num3 + num4 + num5;
+		float promedio = (float) suma / 5;
+		
+		System.out.println("El promedio es " + promedio);
 
 	}
 
